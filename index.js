8e6069b235da5490adf7675bda5b05f729c9a597
dipankar-halder <dipankar15-11137@diu.edu.bd>
2024-09-26 13:49:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// use middleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  'mongodb+srv://e-commerce:02bqMaciMVcIC4ak@cluster0.awdypns.mongodb.net/?retryWrites=true&w=majority';
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const menuCollection = client.db('BistroBoss').collection('menu');
+    const reviewsCollection = client.db('BistroBoss').collection('reviews');
+    const cartsCollection = client.db('BistroBoss').collection('carts');
+
+    // get menu
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get reviews
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // cart post
+    app.post('/carts', async (req, res) => {
+      const result = await cartsCollection.insertOne(req.body);
+      res.send(result);
+    });
+    // get carts
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Sitting bistro boss ');
+});
+
+app.listen(port, () => {
+  console.log('Bistro Boss is setting ');
+});
