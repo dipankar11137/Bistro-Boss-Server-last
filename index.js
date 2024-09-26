b5b5b58074fc97574347d4300c511e3044e8fa44
dipankar-halder <dipankar15-11137@diu.edu.bd>
2024-09-26 16:41:03 +0600

    add delete

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
       const result = await cartsCollection.find(query).toArray();
       res.send(result);
     });
+    // delete
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
