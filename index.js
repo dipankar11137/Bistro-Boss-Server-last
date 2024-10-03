70cb1cb703fd44d12d2320051c30f4a32c305c8d
dipankar-halder <dipankar15-11137@diu.edu.bd>
2024-10-03 15:31:35 +0600

    jwt

--- index.js
+++ index.js
@@ -43,7 +43,13 @@ async function run() {
         return res.status(401).send({ message: 'forbidden access' });
       }
       const token = req.headers.authorization.split(' ')[1];
-      // next();
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+      });
     };
 
     // get users
